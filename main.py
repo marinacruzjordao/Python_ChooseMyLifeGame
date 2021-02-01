6b8fd43fb4f87a4ee97b9a78cfca311393cea628
marinacruzjordao <marinacruzjordao@gmail.com>
2021-02-01 18:03:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Choose My Life Game
+# Write a question and obtain a random answer
+
+class Choose:
+
+    def __init__(self):
+        self.answers=['Yes.', 'No.', 'Sure, why not?', 'Sure, you should do that.', 'I do not know.',
+                      'Do it.','Do not do this.','Yes, is the right time to do it.']
